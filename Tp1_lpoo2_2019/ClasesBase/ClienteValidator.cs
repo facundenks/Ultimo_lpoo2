fc928148b193f542382dfba1ce392fe75dedc166
facundenks <facundenks20@gmail.com>
2019-10-06 22:14:24 -0300

    validator ahora si

--- Tp1_lpoo2_2019/ClasesBase/ClienteValidator.cs
+++ Tp1_lpoo2_2019/ClasesBase/ClienteValidator.cs
@@ -8,6 +8,52 @@ namespace ClasesBase
 {
     public class ClienteValidator: IDataErrorInfo
     {
+        private String cli_dni;
 
+        public String Cli_dni
+        {
+            get { return cli_dni; }
+            set { cli_dni = value; }
+        }
+        private String cli_nombre;
+
+        public String Cli_nombre
+        {
+            get { return cli_nombre; }
+            set { cli_nombre = value; }
+        }
+        private String cli_apellido;
+
+        public String Cli_apellido
+        {
+            get { return cli_apellido; }
+            set { cli_apellido = value; }
+        }
+        private String cli_telefono;
+
+        public String Cli_telefono
+        {
+            get { return cli_telefono; }
+            set { cli_telefono = value; }
+        }
+        private String cli_email;
+
+        public String Cli_email
+        {
+            get { return cli_email; }
+            set { cli_email = value; }
+        }
+
+
+
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string this[string columnName]
+        {
+            get { throw new NotImplementedException(); }
+        }
     }
 }
