7f6f60f6ec66c623b2bfddba735b385f9bd23713
vaninavaldez <vaninavaldez013@gmail.com>
2019-11-14 06:41:44 -0600

    ciudad completa

---
+++ Tp1_lpoo2_2019/ClasesBase/ClassTrabajarCiudadString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClasesBase.DAO.Repositorio;
+
+namespace ClasesBase
+{
+    public class ClassTrabajarCiudadString
+    {
+        public List<ClassCiudadString> CiudadStringList()
+        {
+            List<ClassCiudadString> listaString = new List<ClassCiudadString>(); 
+            CiudadRepositorio _ciudadRepositorio = new CiudadRepositorio();
+            List<Ciudad> ciudades = _ciudadRepositorio.getCiudades();
+            foreach(Ciudad ciu in ciudades)
+            {
+                ClassCiudadString ciudad = new ClassCiudadString();
+                ciudad.Ciu_codigo = Convert.ToString(ciu.ciu_codigo);
+                ciudad.Ciu_nombre = ciu.ciu_nombre;
+                
+                listaString.Add(ciudad);
+            }
+
+            return listaString;
+        }
+    }
+}
